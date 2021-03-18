4714fe34bb092410ef06eec49ebaee44210a4850
swornimmaharjan <sworniimmaharjan@gmail.com>
2021-03-18 11:00:46 +0545

    challenge_push

---
+++ README.md
@@ -0,0 +1,6 @@
+# Coding Challenge
+### App screenshot
+![](./three_button.png)
+![](./text_1.png)
+![](./ttext_2.png)
+![](./text_3.png)
--- app/src/main/java/com/example/twoactivity/MainActivity.java
+++ app/src/main/java/com/example/twoactivity/MainActivity.java
@@ -4,11 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_TEXT = "com.example.activities_and_intent.extra.TEXT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showText(View view) {
+        Intent intent = new Intent(this, Second.class);
+
+//        insert some random text; got from randomtextgenerator.com
+        switch (view.getId()){
+            case R.id.button_one:
+                intent.putExtra(EXTRA_TEXT, getString(R.string.display_one)); startActivity(intent);
+                break;
+            case R.id.button_two:
+                intent.putExtra(EXTRA_TEXT, getString(R.string.display_two)); startActivity(intent);
+                break;
+            case R.id.button_three:
+                intent.putExtra(EXTRA_TEXT, getString(R.string.display_three)); startActivity(intent);
+                break;
+            default:
+                Toast.makeText(MainActivity.this, "There was an error", Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
